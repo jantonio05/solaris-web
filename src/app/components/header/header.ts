7eca9dfd1d65cd24d2bdfa21ad696eb172bb7d7a
josan <jacalderonp05@educarex.es>
2026-05-09 14:16:30 +0200

    feature(header-search): Definida estructura de como se llevará a cabo la búsqueda.

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -9,4 +9,19 @@ import { RouterLink } from "@angular/router";
 })
 export class Header {
 
+  busqueda!: String;
+
+  onKeyDown(event: KeyboardEvent): void {
+    if (event.key != 'Enter') return;
+    
+    // Obtener el texto
+    // Cortar por los dos puntos ':'
+    // Si antes de los dos puntos hay una 'h'
+      // Navegamos hasta /hoy/localidad-a-buscar
+    // ElseIf Tiene una 'p'
+      // Navegamos hasta /hoy/localidad-a-buscar/dias[en el caso de que no haya días, por defecto 7]
+    // Else 
+      // Return
+  }
+
 }
