d3fdfa09cb43f14138a4d23b7b887a247449dc7c
jantonio <jacalderonp05@educarex.es>
2026-05-11 14:27:38 +0200

    feat(prediccion): Ahora se muestra el contenido de prediccion.

--- src/app/pages/prediccion/prediccion.ts
+++ src/app/pages/prediccion/prediccion.ts
@@ -5,10 +5,12 @@ import { formatString } from '../../utils/string.utils';
 import { Footer } from "../../components/footer/footer";
 import { Header } from "../../components/header/header";
 import { ActivatedRoute } from '@angular/router';
+import { NgClass } from '@angular/common';
+import { IDia } from '../../model/IDia';
 
 @Component({
   selector: 'page-prediccion',
-  imports: [Footer, Header],
+  imports: [Footer, Header, NgClass],
   templateUrl: './prediccion.html',
   styleUrl: './prediccion.css',
 })
@@ -33,7 +35,93 @@ export class Prediccion implements OnInit {
 
   obtenerPrediccion(): void {
     this.localidad = formatString(this.localidad!);
-    this.openMeteoService.obtenerPrediccion(this.localidad, this.dias).subscribe((data) => this.prediccion = data);
+    this.openMeteoService.obtenerPrediccion(this.localidad, this.dias).subscribe((data) => {
+      this.prediccion = data
+    });
+  }
+
+  obtenerNumeroColumnas(): String[] {
+    return [`grid-cols-${this.dias}`, `gap-${this.dias}`]
+  }
+
+  obtenerBadgeEstado(dia: IDia): String {
+    switch (dia.estado) {
+      case "soleado":
+        return "Soleado"
+      case "mayormente_soleado":
+        return "Mayormente soleado"
+      case "parcialmente_nublado":
+        return "Parcialmente nublado"
+      case "niebla":
+        return "Niebla"
+      case "llovizna":
+        return "Llovizna"
+      case "lluvia":
+        return "Lluvia"
+      case "chubascos":
+        return "Chubascos"
+      case "tormenta":
+        return "Tormenta"
+      case "nieve":
+        return "Nieve"
+      case "desconocido":
+        return "Desconocido"
+      default:
+        return "Ninguno";
+    }
+  }
+
+  obtenerTipoBadgeEstado(dia: IDia) {
+    switch (dia.estado) {
+      case "soleado":
+      case "mayormente_soleado":
+        return `badge-primary`
+        
+      case "parcialmente_nublado":
+      case "niebla":
+      case "llovizna":
+      case "lluvia":
+      case "chubascos":
+      case "tormenta":
+      case "nieve":
+        return `badge-secondary`;
+
+      case "desconocido":
+        return `badge-info`;
+
+      default:
+        return `badge-error`;
+    }
+  }
+
+  obtenerImagenEstado(dia: IDia): String {
+    switch (dia.estado) {
+      case "soleado":
+      case "mayormente_soleado":
+        return "/estado/sol.png"
+        
+      case "parcialmente_nublado":
+        return "/estado/parcialmente_nublado.png"
+      case "niebla":
+        return "/estado/nublado.png";
+    
+      case "llovizna":
+      case "chubascos":
+        return "/estado/llovizna.png"
+      case "lluvia":
+        return "/estado/lluvia.png"
+      case "tormenta":
+        return "/estado/tormenta.png";
+
+      case "nieve":
+        return "/estado/nieve.png";
+
+      case "desconocido":
+        return "/estado/error.png";
+
+      default:
+        return "/estado/error.png";
+    }
   }
 
 }
